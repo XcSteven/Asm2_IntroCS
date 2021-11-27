6e5e550513349a0c85048e740a28c3e9f177ce01
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-11-27 12:15:04 +0700

    Restart project, completed level 1 of the game

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+def on_sound_loud():
+    led.plot(2, 2)
+    basic.pause(100)
+    led.unplot(2, 2)
+    basic.show_leds("""
+        . . . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                . . . . .
+    """)
+    basic.pause(100)
+    basic.clear_screen()
+    basic.show_leds("""
+        # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+    """)
+    basic.pause(100)
+    basic.clear_screen()
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
-input.onSound(DetectedSound.Loud, function () {
-    led.plot(2, 2)
-    basic.pause(100)
-    led.unplot(2, 2)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.pause(100)
-    basic.clearScreen()
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.pause(100)
-    basic.clearScreen()
+input.onButtonPressed(Button.A, function () {
+    basket.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    basket.change(LedSpriteProperty.X, 1)
+})
+let fruit: game.LedSprite = null
+let basket: game.LedSprite = null
+basket = game.createSprite(2, 4)
+game.setScore(0)
+game.setLife(3)
+basic.forever(function () {
+    fruit = game.createSprite(randint(0, 4), -1)
+    fruit.change(LedSpriteProperty.Brightness, 200)
+    for (let index = 0; index < 5; index++) {
+        basic.pause(500)
+        fruit.change(LedSpriteProperty.Y, 1)
+        if (fruit.isTouching(basket)) {
+            game.addScore(1)
+            fruit.delete()
+        }
+    }
+    if (fruit.isTouchingEdge()) {
+        game.removeLife(1)
+    }
+    fruit.delete()
 })
